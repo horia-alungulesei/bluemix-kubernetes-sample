79d4e13cb825f445095e3cf86869f3b6c525cbb8
Horia Alungulesei <horia.alungulesei@ro.ibm.com>
2017-05-11 17:07:39 +0300

    fix namespace

--- deploy.sh
+++ deploy.sh
@@ -27,7 +27,7 @@ else
   echo -e "App already deployed to cluster, updating it..."
   LATEST_APP_VERSION=$(bx ic images | grep $IOT4I_APP_NAME | sort -rnk2 | awk '!x[$1]++' | awk '{print $2}')
   # set the new version to the deployment, this would perform a red/black update
-  kubectl set image deployment $IOT4I_APP_NAME $IOT4I_APP_NAME=registry.ng.bluemix.net/iot4i/$IOT4I_APP_NAME:$LATEST_APP_VERSION
+  kubectl set image deployment $IOT4I_APP_NAME $IOT4I_APP_NAME=registry.ng.bluemix.net/iot4i_v2/$IOT4I_APP_NAME:$LATEST_APP_VERSION
 fi
 
 PORT=$(kubectl get services | grep $SERVICE_NAME | sed 's/.*://g' | sed 's/\/.*//g')
